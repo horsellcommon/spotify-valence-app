d78df51b2ce33e26042e61edb1cf7050fcd17f44
horsellcommon <crawfordj716@gmail.com>
2023-01-21 18:36:46 +0000

    first commit

---
+++ test.py
@@ -0,0 +1,17 @@
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+import os
+from decouple import config
+
+os.environ["SPOTIPY_CLIENT_ID"] = config("ID")
+os.environ["SPOTIPY_CLIENT_SECRET"] = config("SECRET")
+os.environ["SPOTIPY_REDIRECT_URI"] = config("URI")
+
+scope = "user-library-read"
+
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+
+results = sp.current_user_saved_tracks()
+for idx, item in enumerate(results['items']):
+    track = item['track']
+    print(idx, track['artists'][0]['name'], " – ", track['name'])
