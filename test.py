423a20d336c5815c250b437e346db1897067f9f7
horsellcommon <crawfordj716@gmail.com>
2023-01-21 22:52:32 +0000

    Function to grab playlists

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ from spotipy.oauth2 import SpotifyOAuth
 import os
 from decouple import config
 import pprint
-
+import sys
 os.environ["SPOTIPY_CLIENT_ID"] = config("ID")
 os.environ["SPOTIPY_CLIENT_SECRET"] = config("SECRET")
 os.environ["SPOTIPY_REDIRECT_URI"] = config("URI")
@@ -12,15 +12,18 @@ os.environ["SPOTIPY_REDIRECT_URI"] = config("URI")
 yes = ["Y", "y"]
 no = ["N", "n"]
 
+username = input("Enter username: ")
+client_credentials_manager = SpotifyClientCredentials()
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager, auth_manager=SpotifyOAuth())
+user = sp.user(username)
+
 def gather_data():
+    global username
     gather_q = ""
     while gather_q not in yes or no:
         gather_q = input("Gather user data? Y/N ")
+        print("---------------------")
         if gather_q in yes:
-            username = input("Enter username: ")
-            client_credentials_manager = SpotifyClientCredentials()
-            sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
-            user = sp.user(username)
             pprint.pprint(user)
             break
         elif gather_q in no:
@@ -28,4 +31,23 @@ def gather_data():
             break
         else:
             print("Command not recognised.")
+def retrieve_playlist():
+    global username
+    retrieve_list = ""
+    while retrieve_list not in yes or no:
+        retrieve_list = input("Gather playlist data? Y/N ")
+        print("---------------------")
+        if retrieve_list in yes:
+            playlists = sp.user_playlists(username)
+            for playlist in playlists['items']:
+                print(playlist['name'])
+            break
+        elif retrieve_list in no:
+            print("Exiting.")
+            sys.exit()
+        else:
+            print("Command not recognised.")
+
 gather_data()
+print("---------------------")
+retrieve_playlist()
