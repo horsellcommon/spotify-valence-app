be89562d3c31380e3b8fe4b10cbff0cf6e4f7fe5
horsellcommon <crawfordj716@gmail.com>
2023-01-21 21:04:39 +0000

    Made gather data into function and made optional

--- test.py
+++ test.py
@@ -9,9 +9,23 @@ os.environ["SPOTIPY_CLIENT_ID"] = config("ID")
 os.environ["SPOTIPY_CLIENT_SECRET"] = config("SECRET")
 os.environ["SPOTIPY_REDIRECT_URI"] = config("URI")
 
-username = input("Enter username: ")
+yes = ["Y", "y"]
+no = ["N", "n"]
 
-client_credentials_manager = SpotifyClientCredentials()
-sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
-user = sp.user(username)
-pprint.pprint(user)
+def gather_data():
+    gather_q = ""
+    while gather_q not in yes or no:
+        gather_q = input("Gather user data? Y/N ")
+        if gather_q in yes:
+            username = input("Enter username: ")
+            client_credentials_manager = SpotifyClientCredentials()
+            sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+            user = sp.user(username)
+            pprint.pprint(user)
+            break
+        elif gather_q in no:
+            print("Proceeding.")
+            break
+        else:
+            print("Command not recognised.")
+gather_data()
