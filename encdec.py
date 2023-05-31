8e09ee9de8d13cf826dcb018bf5d4081a4eb8e5a
horsellcommon <crawfordj716@gmail.com>
2023-05-31 18:40:07 +0100

    Started work on separate encrypt/decrypt program for any files

---
+++ encdec.py
@@ -0,0 +1,21 @@
+import sys
+from cryptography.fernet import Fernet
+from pathlib import Path
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
+
+key_grabber = Path("./cryptkey.key")
+output_file = Path("./output.json")
+playlist_file = Path("./playlist.json")
+
+if key_grabber.is_file():
+    with open('cryptkey.key', 'rb') as cryptkey:
+        key = cryptkey.read()
+    fernet = Fernet(key)
+else:
+    sys.exit()
+
+print("Please select an output file to encrypt.")
+Tk().withdraw()
+output_file = Path(askopenfilename())
+print(output_file)
