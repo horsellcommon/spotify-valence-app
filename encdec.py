d7d147372afa4318fbf24507e71a5f56bb2621a9
horsellcommon <crawfordj716@gmail.com>
2023-05-31 19:11:13 +0100

    Labels and buttons

--- encdec.py
+++ encdec.py
@@ -15,11 +15,22 @@ if key_grabber.is_file():
         key = cryptkey.read()
     fernet = Fernet(key)
 else:
-    sys.exit()
+    key = Fernet.generate_key()
+    with open('cryptkey.key', 'wb') as cryptkey:
+        cryptkey.write(key)
+    fernet = Fernet(key)
 
-print("Please select an output file to encrypt.")
 gui.geometry("600x300")
 gui.title("Spotify Valence and Arousal Tool - Encrypt/Decrypt Files")
+
+outer_box = ttk.Frame(gui, padding="3 3 12 12")
+outer_box.grid(column=0, row=0, sticky=(N, W, E, S))
+gui.columnconfigure(0, weight=1)
+gui.rowconfigure(0, weight=1)
+
+ttk.Label(outer_box, text="Select your playlist output file.").grid(column=1, row=1, sticky=W)
+output_path_select = ttk.Button(outer_box, text="Choose a file.", width=7)
+output_path_select.grid(column=1, row=2, sticky=(W, E))
 input()
 # output_file = Path(askopenfilename())
 # print(output_file)
