f65cf75168380186dd68e2f1d59f3bd8bf1e1981
horsellcommon <crawfordj716@gmail.com>
2023-05-31 19:54:48 +0100

    Encrypt/decrypt selected output/playlist files

--- encdec.py
+++ encdec.py
@@ -17,6 +17,40 @@ def select_output():
 def select_playlist():
     global playlist_file
     playlist_file = Path(askopenfilename())
+def encrypt_output():
+    if output_file.is_file():
+        with open(output_file, "rb") as file:
+            original = file.read()
+        encrypt_data = fernet.encrypt(original)
+        with open(output_file, "wb") as encrypted_file:
+            encrypted_file.write(encrypt_data)
+    else:
+        # Throw a popup or something
+        sys.exit()
+def decrypt_output():
+    if output_file.is_file():
+        with open(output_file, "rb") as encrypted_file:
+            encrypted = encrypted_file.read()
+        decrypted = fernet.decrypt(encrypted)
+        with open(output_file, "wb") as decrypted_output_data:
+            decrypted_output_data.write(decrypted)
+def encrypt_playlist():
+    if playlist_file.is_file():
+        with open(playlist_file, "rb") as file:
+            original = file.read()
+        encrypt_data = fernet.encrypt(original)
+        with open(playlist_file, "wb") as encrypted_file:
+            encrypted_file.write(encrypt_data)
+    else:
+        # Throw a popup or something
+        sys.exit()
+def decrypt_playlist():
+    if playlist_file.is_file():
+        with open(playlist_file, "rb") as encrypted_file:
+            encrypted = encrypted_file.read()
+        decrypted = fernet.decrypt(encrypted)
+        with open(playlist_file, "wb") as decrypted_playlist_data:
+            decrypted_playlist_data.write(decrypted)
 
 if key_grabber.is_file():
     with open('cryptkey.key', 'rb') as cryptkey:
@@ -40,11 +74,19 @@ gui.rowconfigure(0, weight=1)
 ttk.Label(outer_box, text="Select your songs output file.").grid(column=1, row=1, sticky=W)
 output_path_select = ttk.Button(outer_box, text="Choose a file.", command=select_output, width=7)
 output_path_select.grid(column=1, row=2, sticky=(W, E))
+encrypt_output_button = ttk.Button(outer_box, text="ENCRYPT", command=encrypt_output, width=7)
+encrypt_output_button.grid(column=1, row=3, sticky=(W, E))
+decrypt_output_button = ttk.Button(outer_box, text="DECRYPT", command=decrypt_output, width=7)
+decrypt_output_button.grid(column=1, row=4, sticky=(W, E))
 
 # Playlist file encrypt/decrypt
 ttk.Label(outer_box, text="Select your playlist output file.").grid(column=2, row=1, sticky=W)
-output_path_select = ttk.Button(outer_box, text="Choose a file.", command=select_playlist, width=7)
-output_path_select.grid(column=2, row=2, sticky=(W, E))
+playlist_path_select = ttk.Button(outer_box, text="Choose a file.", command=select_playlist, width=7)
+playlist_path_select.grid(column=2, row=2, sticky=(W, E))
+encrypt_playlist_button = ttk.Button(outer_box, text="ENCRYPT", command=encrypt_playlist, width=7)
+encrypt_playlist_button.grid(column=2, row=3, sticky=(W, E))
+decrypt_playlist_button = ttk.Button(outer_box, text="DECRYPT", command=decrypt_playlist, width=7)
+decrypt_playlist_button.grid(column=2, row=4, sticky=(W, E))
 
 input()
 print(output_file)
