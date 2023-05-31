536e2e7270dcc5e68445116becbccc683216b06e
horsellcommon <crawfordj716@gmail.com>
2023-05-31 19:35:19 +0100

    Select output and playlist files

--- encdec.py
+++ encdec.py
@@ -9,11 +9,14 @@ gui = Tk()
 
 key_grabber = Path("./cryptkey.key")
 output_file = None
-playlist_file = Path("./playlist.json")
+playlist_file = None
 
 def select_output():
     global output_file
     output_file = Path(askopenfilename())
+def select_playlist():
+    global playlist_file
+    playlist_file = Path(askopenfilename())
 
 if key_grabber.is_file():
     with open('cryptkey.key', 'rb') as cryptkey:
@@ -33,11 +36,19 @@ outer_box.grid(column=0, row=0, sticky=(N, W, E, S))
 gui.columnconfigure(0, weight=1)
 gui.rowconfigure(0, weight=1)
 
-ttk.Label(outer_box, text="Select your playlist output file.").grid(column=1, row=1, sticky=W)
+# Output file encrypt/decrypt
+ttk.Label(outer_box, text="Select your songs output file.").grid(column=1, row=1, sticky=W)
 output_path_select = ttk.Button(outer_box, text="Choose a file.", command=select_output, width=7)
 output_path_select.grid(column=1, row=2, sticky=(W, E))
+
+# Playlist file encrypt/decrypt
+ttk.Label(outer_box, text="Select your playlist output file.").grid(column=2, row=1, sticky=W)
+output_path_select = ttk.Button(outer_box, text="Choose a file.", command=select_playlist, width=7)
+output_path_select.grid(column=2, row=2, sticky=(W, E))
+
 input()
 print(output_file)
+print(playlist_file)
 # output_file = Path(askopenfilename())
 # print(output_file)
 # sys.exit()
